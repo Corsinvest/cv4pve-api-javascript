c3bbbf07c7bc85540d4c0fde8c834403607e9684
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2024-02-15 11:09:20 +0100

    Improve internal send

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const pve = require('./src');
 console.log(pve);
 
 async function foo() {
-    const client = new pve.PveClient('10.92.90.101', 8006);
+    const client = new pve.PveClient('192.168.0.2', 8006);
     //client.logEnabled = true;
     //client.apiToken = '';
 
@@ -15,14 +15,17 @@ async function foo() {
         console.log((await client.get('/nodes')).response);
         console.log((await client.nodes.index()).response);
 
-        console.log((await client.get('/nodes/cv-pve01/qemu')).response);
-        console.log((await client.nodes.get('cv-pve01').qemu.vmlist(0)).response);
+        let result = await client.nodes.get("cc01").qemu.get(1006).agent.exec.exec(["powershell", "-command", "echo", "test"]);
+        console.log(result.response);
 
-        console.log((await client.get('/nodes/cv-pve01/qemu/103/config/')).response);
-        console.log((await client.nodes.get('cv-pve01').qemu.get(103).config.vmConfig()).response);
+        // console.log((await client.get('/nodes/cv-pve01/qemu')).response);
+        // console.log((await client.nodes.get('cv-pve01').qemu.vmlist(0)).response);
 
-        console.log((await client.get('/nodes/cv-pve01/qemu/103/snapshot/')).response);
-        console.log((await client.nodes.get('cv-pve01').qemu.get(103).snapshot.snapshotList()).response);
+        // console.log((await client.get('/nodes/cv-pve01/qemu/103/config/')).response);
+        // console.log((await client.nodes.get('cv-pve01').qemu.get(103).config.vmConfig()).response);
+
+        // console.log((await client.get('/nodes/cv-pve01/qemu/103/snapshot/')).response);
+        // console.log((await client.nodes.get('cv-pve01').qemu.get(103).snapshot.snapshotList()).response);
     }
 }
 
